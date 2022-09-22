f2c7b4b5212f701464ca8b321ca07eab29e4baa0
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2022-09-22 13:18:40 +0530

    parameters

--- src/test/java/com/demo/mavenone/Demo.java
+++ src/test/java/com/demo/mavenone/Demo.java
@@ -5,27 +5,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo {
 
+	@Parameters({"Browser"})
 	@Test
-	public void hello() throws InterruptedException {
-		System.out.println("maven: hello");
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.gmail.com");
-		WebElement un = driver.findElement(By.id("identifierId"));
-		Assert.assertTrue(un.isDisplayed()&&un.isEnabled());
-		un.sendKeys("java");
-		//Thread.sleep(5000);
-		//WebDriverManager.chromedriver().create();
-		System.out.println("@Test: hello()");
-		Thread.sleep(5000);
-		driver.quit();
+	public void hello(String browser) throws InterruptedException {
+		
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.out.println("maven: hello");
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.gmail.com");
+			WebElement un = driver.findElement(By.id("identifierId"));
+			Assert.assertTrue(un.isDisplayed()&&un.isEnabled());
+			un.sendKeys("java");
+			//Thread.sleep(5000);
+			//WebDriverManager.chromedriver().create();
+			System.out.println("@Test: hello()");
+			System.out.println("BROWSER: "+browser);
+			Thread.sleep(5000);
+			driver.quit();
+		}
+		else {
+			System.out.println("Hello: "+browser);
+		}
+		
+		
 	}
 	
 }
